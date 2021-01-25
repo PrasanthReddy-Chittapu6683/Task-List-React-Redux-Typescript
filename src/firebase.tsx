ba4b81860c93d32dab3ca9b9b17f64a29d015155
Prasanth Reddy CV <PrasanthReddy-Chittapu@github.com>
2021-01-25 19:14:18 +0530

    deploying to Firebase

---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8EvJdGX9gdGJlL7leQ17jE8dk34N8X1c",
+  authDomain: "task-list-d20ba.firebaseapp.com",
+  projectId: "task-list-d20ba",
+  storageBucket: "task-list-d20ba.appspot.com",
+  messagingSenderId: "27917438355",
+  appId: "1:27917438355:web:f9ed5f27e3a7c028506c66",
+  measurementId: "G-1V83NECRPL"
+};
+
+export default firebaseConfig;
